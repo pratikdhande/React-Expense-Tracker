af471ff46c7bb63a9ae50b3b7ea0ac2e2f034074
pratikdhande <pratikdhande99@gmail.com>
2022-10-29 21:58:26 +0530

    Expense Trackers files added

---
+++ src/context/AppReducer.js
@@ -0,0 +1,20 @@
+// Reducer basically we specify state changes in response to certain action
+
+const AppReducer = (state, action) => {
+    switch(action.type) {
+        case 'DELETE_TRANSACTION':
+            return{
+                ...state,
+                transactions: state.transactions.filter(transaction => transaction.id !== action.payload)
+            }
+        case 'ADD_TRANSACTION':
+            return{
+                ...state,
+                transaction: [action.payload, ...state.transaction]
+            }
+        default:
+            return state;
+    }
+}
+
+export default AppReducer
